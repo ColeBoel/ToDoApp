de250615063d3e13734230425bdb2be90b6494d9
Joshua Boelman <joshcboelman@gmail.com>
2023-03-02 09:23:11 -0500

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Joshua Boelman on 3/2/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
